6ed6703f626772efb4462408d5007e0ac9e1e569
Mateusz <mat.bereda@gmail.com>
2024-11-05 14:26:11 +0100

    Jakis commit

--- src/main/java/pl/edu/wszib/git/App.java
+++ src/main/java/pl/edu/wszib/git/App.java
@@ -2,6 +2,8 @@ package pl.edu.wszib.git;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("GIT");
+        System.out.println("GIT2");
+
+        System.out.println("jakas zmiana");
     }
 }
